3c6df0e82c6c6e7f4aad7a18e9e449984a1c125f
aidenbull <aidenbull@hotmail.com>
2023-12-11 15:46:40 -0800

    Removed fileio from module and added it to separate github repo so I can reuse it

--- day-1/main.go
+++ day-1/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"strings"
+	"github.com/aidenbull/fileio"
 )
 
 func main() {
-	file := OpenFile("input")
-	fileString := strings.ToLower(ReadFile(file))
+	file := fileio.OpenFile("input")
+	fileString := strings.ToLower(fileio.ReadFile(file))
 	// Trim any trailing and leading whitespace before splitting. It appears the input has one trailing newline
 	tokens := strings.Split(strings.TrimSpace(fileString), "\n")
 	PrintPartOneSolution(tokens)
