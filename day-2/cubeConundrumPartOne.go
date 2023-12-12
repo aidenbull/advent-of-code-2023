a5ba45776e8c5c6b71f50ab3a1a90d555dac8901
aidenbull <aidenbull@hotmail.com>
2023-12-12 14:47:07 -0800

    Finished solution to part 1

--- day-2/cubeConundrumPartOne.go
+++ day-2/cubeConundrumPartOne.go
@@ -4,6 +4,7 @@ import (
 	"regexp"
 	"strings"
 	"strconv"
+	"fmt"
 )
 
 type CubeSet struct {
@@ -72,7 +73,7 @@ func processGameNum(gameInput string) int {
 
 // Takes a single line of game input as a string and returns a
 // struct containing the information about that game
-func ProcessGame(gameInput string) CubeGame {
+func processGame(gameInput string) CubeGame {
 	var game CubeGame
 	game.GameNum = processGameNum(gameInput)
 	inputRunes := []rune(gameInput)
@@ -84,4 +85,41 @@ func ProcessGame(gameInput string) CubeGame {
 	game.Sets = processSets(string(setsRunes))
 
 	return game
+}
+
+func preProcessGame(input string) string {
+	return strings.TrimSpace(strings.ToLower(input))
+}
+
+func gameIsValid(game CubeGame) bool {
+	const redMax = 12
+	const greenMax = 13
+	const blueMax = 14
+	for _, set := range game.Sets {
+		if set.Red > redMax {
+			return false
+		}
+		if set.Green > greenMax {
+			return false
+		}
+		if set.Blue > blueMax {
+			return false
+		}
+	}
+	return true
+}
+
+func PrintPartOneSolution(input string) {
+	processedInput := preProcessGame(input)
+
+	tokens := strings.Split(processedInput, "\n")
+	sum := 0
+	for _, token := range tokens {
+		game := processGame(token)
+		if gameIsValid(game) {
+			sum += game.GameNum
+		}
+	}
+
+	fmt.Println(sum)
 }
--- day-2/main.go
+++ day-2/main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/aidenbull/fileio"
 )
 
 func main() {
 	file := fileio.OpenFile("input")
-	fileString := strings.TrimSpace(strings.ToLower(fileio.ReadFile(file)))
-	tokens := strings.Split(fileString, "\n")
-	for _, token := range tokens {
-		fmt.Println(ProcessGame(token))
-	}
+	fileString := fileio.ReadFile(file)
+	PrintPartOneSolution(fileString)
 }
