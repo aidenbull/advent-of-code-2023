3191be5cae3bb2a33f4d61d2ab1f84ca207309f5
aidenbull <aidenbull@hotmail.com>
2023-12-14 16:31:52 -0800

    Passed first test!

--- day-3/runeGrid.go
+++ day-3/runeGrid.go
@@ -1,10 +1,24 @@
 package main
 
 import (
-
+	"unicode"
 )
 
+type BadConversion struct {
+	s string
+}
 
+func (e *BadConversion) Error() string {
+	return e.s
+}
+
+func ConvertRuneToInteger(r rune) (int, error) {
+	isDigit := unicode.IsDigit(r)
+	if !isDigit {
+		return 0, &BadConversion{"Input is not a digit!"}
+	}
+	return int(r - '0'), nil
+}
 
 func ConvertRunesToInteger(runes []rune) (int, error) {
 
--- day-3/runeGrid_test.go
+++ day-3/runeGrid_test.go
@@ -4,20 +4,21 @@ import (
 	"testing"
 )
 
-func TestConvertRuneToNumeral(t *testing.T) {
+func TestConvertSingleRuneToNumeral(t *testing.T) {
 	const testInput1 rune = '3'
-	expectedResult1, expectedError1 := 3, nil
-	result1, error1 := ConvertRuneToInteger() {
-		
+	expectedResult1, expectedError1 := 3, error(nil)
+	result1, error1 := ConvertRuneToInteger(testInput1)
+	if (result1 != expectedResult1) || (error1 != expectedError1) {
+		t.Errorf("Expected (%d, %d), got (%d, %d)", expectedResult1, expectedError1, result1, error1)
 	}
 }
 
-func TestConvertRunesToInteger(t *testing.T) {
-	testInput := []rune{'3', '1'}
-	expected := 31
-	result, _ := ConvertRunesToInteger(testInput)
+// func TestConvertRunesToInteger(t *testing.T) {
+// 	testInput := []rune{'3', '1'}
+// 	expected := 31
+// 	result, _ := ConvertRunesToInteger(testInput)
 
-	if expected != result {
-		t.Errorf("Expected %d, got %d", expected, result)
-	}
-}
+// 	if expected != result {
+// 		t.Errorf("Expected %d, got %d", expected, result)
+// 	}
+// }
