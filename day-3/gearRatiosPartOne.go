501b7f14bb8c08c78267ea7355dbb74d6fb3cfc7
aidenbull <aidenbull@hotmail.com>
2023-12-19 16:01:49 -0800

    Changing index variable from i to j to indicate row rather than column

--- day-3/gearRatiosPartOne.go
+++ day-3/gearRatiosPartOne.go
@@ -7,10 +7,10 @@ import (
 func PrintPartOneSolution(input string) {
 	runeGrid := ConvertStringToRuneGrid(input)
 	sum := 0
-	for i, line := range runeGrid {
+	for j, line := range runeGrid {
 		numberCoords := GetNumberCoordsFromRuneSlice(line)
 		for _, num := range numberCoords {
-			if NumberIsAdjacentToSymbol(runeGrid, i, num.Start, num.End) {
+			if NumberIsAdjacentToSymbol(runeGrid, j, num.Start, num.End) {
 				foundInt, _ := ConvertRunesToInteger(line[num.Start:num.End])
 				sum += foundInt
 			}
