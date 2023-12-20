d66e5a02247c78bf4ad1abc0d49ff52223133b88
aidenbull <aidenbull@hotmail.com>
2023-12-19 17:11:55 -0800

    Second test. This one feels like it'll be faster than day 3

--- day-4/scratchCardGame_test.go
+++ day-4/scratchCardGame_test.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"testing"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestReadScratchCardGameFromString(t *testing.T) {
 	testInput := "Card   1: 43 19 57 | 34 68 13 38"
 
-	expected := ScratchGame{WinningNums:{43, 19, 57}, YourNums:{34, 68, 13, 38}}
+	expected := ScratchGame{[]int{43, 19, 57}, []int{34, 68, 13, 38}}
 	result := ScratchGameFromString(testInput)
 
-	if expected != result {
+	if !cmp.Equal(expected, result) {
 		t.Errorf("Expected %+v, got %+v", expected, result)
+	}
+}
+
+func TestEvaluateGameValueWithValueZero(t *testing.T) {
+	testInput := ScratchGame{[]int{1, 2, 3}, []int{4, 5, 6, 7}}
+
+	expected := 0
+	result := EvaluateGame(testInput)
+
+	if expected != result {
+		t.Errorf("Expected %v, got %v", expected, result)
 	}
 }
