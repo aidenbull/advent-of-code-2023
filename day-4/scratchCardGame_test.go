2e712474f4db4d551d3d535082aeedcb1df8501e
aidenbull <aidenbull@hotmail.com>
2023-12-19 16:44:41 -0800

    First test for day 4

--- day-4/scratchCardGame_test.go
+++ day-4/scratchCardGame_test.go
@@ -3,3 +3,14 @@ package main
 import (
 	"testing"
 )
+
+func TestReadScratchCardGameFromString(t *testing.T) {
+	testInput := "Card   1: 43 19 57 | 34 68 13 38"
+
+	expected := ScratchGame{WinningNums:{43, 19, 57}, YourNums:{34, 68, 13, 38}}
+	result := ScratchGameFromString(testInput)
+
+	if expected != result {
+		t.Errorf("Expected %+v, got %+v", expected, result)
+	}
+}
