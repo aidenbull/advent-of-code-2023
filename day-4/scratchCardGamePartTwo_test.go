66af94a3bdda7a65848c74ef0af689fc1405a93b
aidenbull <aidenbull@hotmail.com>
2023-12-26 19:26:29 -0800

    Added test with two starting cards. Also renamed previous test

--- day-4/scratchCardGamePartTwo_test.go
+++ day-4/scratchCardGamePartTwo_test.go
@@ -4,12 +4,27 @@ import (
 	"testing"
 )
 
-func TestPartTwoOneCard(t *testing.T) {
+func TestPartTwoOneStartingCard(t *testing.T) {
 	testInput := []ScratchGame {
 		{[]int{1,2,3}, []int{1, 4, 5, 6}},
 	}
 
 	expected := 1
+	result := CalculatePartTwo(testInput)
+
+	if expected != result {
+		t.Errorf("Expected %v, got %v", expected, result)
+	}
+}
+
+
+func TestPartTwoTwoStartingCards(t *testing.T) {
+	testInput := []ScratchGame {
+		{[]int{1,2,3}, []int{1, 4, 5, 6}},
+		{[]int{4,5,6}, []int{1, 4, 5, 6}},
+	}
+
+	expected := 3
 	result := CalculatePartTwo(testInput)
 
 	if expected != result {
