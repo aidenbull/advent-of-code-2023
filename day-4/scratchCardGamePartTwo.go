1abc6cfb0548d3ee39a7feb560d838672cac0c03
aidenbull <aidenbull@hotmail.com>
2023-12-26 19:29:36 -0800

    Passing second test

--- day-4/scratchCardGamePartTwo.go
+++ day-4/scratchCardGamePartTwo.go
@@ -3,6 +3,14 @@ package main
 import (
 )
 
-func CalculatePartTwo(game []ScratchGame) int {
-	return len(game)
+func CalculatePartTwo(games []ScratchGame) int {
+	sum := 0
+	matches := make([]int, 0)
+
+	for _, game := range games {
+		sum += 1 + len(matches)
+		matches = append(matches, EvaluateGameMatches(game))
+	}
+	
+	return sum
 }
