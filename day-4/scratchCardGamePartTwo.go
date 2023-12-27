6e47f329ffc86b93913ac0d0059ecf7aa3ea1e48
aidenbull <aidenbull@hotmail.com>
2023-12-27 12:54:34 -0800

    Completed test number 3. Might have a complete solution at this point, and hopefully with a good runtime as well

--- day-4/scratchCardGamePartTwo.go
+++ day-4/scratchCardGamePartTwo.go
@@ -3,13 +3,36 @@ package main
 import (
 )
 
+type ActiveDuplication struct {
+	DuplicateQueueSize int
+	NumberOfDuplicates int
+}
+
 func CalculatePartTwo(games []ScratchGame) int {
 	sum := 0
-	matches := make([]int, 0)
+	activeDuplications := make([]ActiveDuplication, 0) 
 
 	for _, game := range games {
-		sum += 1 + len(matches)
-		matches = append(matches, EvaluateGameMatches(game))
+		//There is always at least 1 card (the original) in each game
+		currentMultiples := 1 
+
+		//Determine number of cards at this stage, and update the active duplications
+		nextActiveDuplications := make([]ActiveDuplication, 0)
+		for _, duplication := range activeDuplications {
+			currentMultiples += duplication.NumberOfDuplicates
+			duplication.DuplicateQueueSize--
+			if duplication.DuplicateQueueSize > 0 {
+				nextActiveDuplications = append(nextActiveDuplications, duplication)
+			}
+		}
+		activeDuplications = nextActiveDuplications
+
+		sum += currentMultiples
+		
+		matches := EvaluateGameMatches(game)
+		if matches > 0 {
+			activeDuplications = append(activeDuplications, ActiveDuplication{matches, currentMultiples}) 
+		}
 	}
 	
 	return sum
