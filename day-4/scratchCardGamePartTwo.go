0f309d186cf2428d94881f15256874100478523c
aidenbull <aidenbull@hotmail.com>
2023-12-27 13:14:21 -0800

    Refactor: Extract method for calculating current number of cards

--- day-4/scratchCardGamePartTwo.go
+++ day-4/scratchCardGamePartTwo.go
@@ -8,18 +8,27 @@ type ActiveDuplication struct {
 	NumberOfDuplicates int
 }
 
+func calculateCurrentNumberOfCards(activeDuplications []ActiveDuplication) int {
+	currentMultiples := 1
+
+	for _, duplication := range activeDuplications {
+		currentMultiples += duplication.NumberOfDuplicates
+	}
+
+	return currentMultiples
+}
+
 func CalculatePartTwo(games []ScratchGame) int {
 	sum := 0
 	activeDuplications := make([]ActiveDuplication, 0) 
 
 	for _, game := range games {
 		//There is always at least 1 card (the original) in each game
-		currentMultiples := 1 
+		currentMultiples := calculateCurrentNumberOfCards(activeDuplications)
 
 		//Determine number of cards at this stage, and update the active duplications
 		nextActiveDuplications := make([]ActiveDuplication, 0)
 		for _, duplication := range activeDuplications {
-			currentMultiples += duplication.NumberOfDuplicates
 			duplication.DuplicateQueueSize--
 			if duplication.DuplicateQueueSize > 0 {
 				nextActiveDuplications = append(nextActiveDuplications, duplication)
