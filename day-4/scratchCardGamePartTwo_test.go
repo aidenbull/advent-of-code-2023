08f6e10a1cc25d1fd0d80547b66c3df19e2c331b
aidenbull <aidenbull@hotmail.com>
2023-12-27 13:05:15 -0800

    Added a complicated test to check for removing items. Perhaps too complicated for a unit test. Nonetheless the solution passed

--- day-4/scratchCardGamePartTwo_test.go
+++ day-4/scratchCardGamePartTwo_test.go
@@ -46,3 +46,20 @@ func TestPartTwoThreeStartingCards(t *testing.T) {
 		t.Errorf("Expected %v, got %v", expected, result)
 	}
 }
+
+func TestPartTwoWithDroppedDuplicates(t *testing.T) {
+	testInput := []ScratchGame {
+		{[]int{1,2}, []int{1, 2}}, // 2 matches, 1 card 
+		{[]int{4,5,6}, []int{4, 5, 6}}, // 3 matches, 2 cards
+		{[]int{7}, []int{7}}, // 1 match, 4 cards
+		{[]int{7}, []int{1}}, // 0 matches, 7 cards
+		{[]int{1}, []int{2}}, // 0 matches, 3 cards
+	} //TOTAL : 17 cards
+
+	expected := 17
+	result := CalculatePartTwo(testInput)
+
+	if expected != result {
+		t.Errorf("Expected %v, got %v", expected, result)
+	}
+}
