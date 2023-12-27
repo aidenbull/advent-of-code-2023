41c3637cd100eccfab31e193ce4844fe1c1608e0
aidenbull <aidenbull@hotmail.com>
2023-12-27 13:17:44 -0800

    Refactor: Extract Method. Decrementing and removing items from activeDuplications

--- day-4/scratchCardGamePartTwo.go
+++ day-4/scratchCardGamePartTwo.go
@@ -18,25 +18,27 @@ func calculateCurrentNumberOfCards(activeDuplications []ActiveDuplication) int {
 	return currentMultiples
 }
 
+func decrementAndRemoveActiveDuplications(activeDuplications []ActiveDuplication) []ActiveDuplication {
+	nextActiveDuplications := make([]ActiveDuplication, 0)
+	for _, duplication := range activeDuplications {
+		duplication.DuplicateQueueSize--
+		if duplication.DuplicateQueueSize > 0 {
+			nextActiveDuplications = append(nextActiveDuplications, duplication)
+		}
+	}
+	return nextActiveDuplications
+}
+
 func CalculatePartTwo(games []ScratchGame) int {
 	sum := 0
 	activeDuplications := make([]ActiveDuplication, 0) 
 
 	for _, game := range games {
 		currentMultiples := calculateCurrentNumberOfCards(activeDuplications)
-
-		//Determine number of cards at this stage, and update the active duplications
-		nextActiveDuplications := make([]ActiveDuplication, 0)
-		for _, duplication := range activeDuplications {
-			duplication.DuplicateQueueSize--
-			if duplication.DuplicateQueueSize > 0 {
-				nextActiveDuplications = append(nextActiveDuplications, duplication)
-			}
-		}
-		activeDuplications = nextActiveDuplications
-
 		sum += currentMultiples
-		
+
+		activeDuplications = decrementAndRemoveActiveDuplications(activeDuplications)
+
 		matches := EvaluateGameMatches(game)
 		if matches > 0 {
 			activeDuplications = append(activeDuplications, ActiveDuplication{matches, currentMultiples}) 
