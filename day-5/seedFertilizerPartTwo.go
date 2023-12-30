b16131732eae1a34b2a8bc7484cea9bba319fee6
aidenbull <aidenbull@hotmail.com>
2023-12-30 12:53:12 -0800

    Working on a solution to identify only the seeds that might possibly be the smallest by finding seed ranges that overlap map ranges. Stopping for the moment because I realize that after multiple iterations of maps, the true lowest value might be in the middle of these overlapped ranges

--- day-5/seedFertilizerPartTwo.go
+++ day-5/seedFertilizerPartTwo.go
@@ -2,16 +2,92 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
-func GetLowestLocationFromSeedRange(start, length int, mapSets []MapSet) int {
-	lowestLocation := ApplyMultipleMaps(start, mapSets)
-	for i := start + 1 ; i < start + length ; i++ {
-		currLocation := ApplyMultipleMaps(i, mapSets)
-		if currLocation < lowestLocation {
-			lowestLocation = currLocation
+type InterestRange struct {
+	Start int,
+	End int,
+}
+
+func max(a, b) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func less(a, b InterestRange) bool {
+	return a.Start < b.Start
+}
+
+func mapIsApplicable(seedStart, length int, m FertilizerMap) bool {
+	seedEnd := seedStart + length
+	mapEnd := m.SrcStart + m.Length
+
+	return seedStart < mapEnd && seedEnd > m.SrcStart, lastStart, firstEnd
+}
+
+//Returns the first and last seed from the given range that is mapped by m
+func seedsOfInterest(seedStart, length int, m FertilizerMap) InterestRange {
+	seedEnd := seedStart + length
+	mapEnd := m.SrcStart + m.Length
+
+	lastStart := max(seedStart, m.SrcStart)
+	firstEnd := min(seedEnd, mapEnd)
+
+	return {lastStart, firstEnd}
+}
+
+func getSortedInterestRanges(seedStart, length int, maps []FertilizerMap) []InterestRange {
+	interestRanges := make([]InterestRange, 0)
+	for _, m := maps {
+		interestRanges = append(interestRanges, seedsOfInterest(seedStart, length, m))
+	}
+	sort.Slice(interestRanges, func (i, j int) bool {
+		return less(interestRanges[i], interestRanges[j])
+	})
+
+	return interestRanges
+}
+
+//Bool returns true if there is an unmapped seed. The int returns the smallest unmapped seed (or 0 if doesn't exist)
+// Relies on the assumption that no maps overlap the same domain
+func smallestUnmappedSeed(seedStart, length int, sortedInterestRanges []InterestRange) {bool, int} {
+	out := seedStart
+	for _, iRange := range sortedInterestRanges {
+		if out < iRange.Start {
+			return true, out
+		} else {
+			out := min(seedStart, iRange.End)
 		}
 	}
+	if out >= seedStart + length {
+		return false, 0
+	}
+	return true, out
+}
+
+func getApplicableMaps(seedStart, length int, mapSet MapSet) MapSet {
+	end := seedStart + length
+	applicableMaps := make([]FertilizerMap, 0)
+
+	for _, m := range MapSet.Maps {
+		if 
+	}
+} 
+
+func ApplySeedRangeToMultipleMaps(start, length int, mapSets []MapSet) int {
+	lowestLocation := ApplyMultipleMaps(start, mapSets)
+
+
 
 	return lowestLocation
 }
@@ -19,13 +95,13 @@ func GetLowestLocationFromSeedRange(start, length int, mapSets []MapSet) int {
 func PrintPartTwoSolution(input string) {
 	seedsAndMaps := ParseSeedsAndMaps(input)
 
-	lowestLocation := GetLowestLocationFromSeedRange(
+	lowestLocation := ApplySeedRangeToMultipleMaps(
 		seedsAndMaps.Seeds[0], 
 		seedsAndMaps.Seeds[1], 
 		seedsAndMaps.MapSets)
 
 	for i := 2 ; i < len(seedsAndMaps.Seeds) ; i += 2 {
-		currLocation := GetLowestLocationFromSeedRange(
+		currLocation := ApplySeedRangeToMultipleMaps(
 			seedsAndMaps.Seeds[i], 
 			seedsAndMaps.Seeds[i + 1], 
 			seedsAndMaps.MapSets)
