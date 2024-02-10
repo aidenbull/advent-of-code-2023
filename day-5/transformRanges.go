1542483dd45a55914a41b3fe9f3aa068a12ad57d
aidenbull <aidenbull@hotmail.com>
2024-02-09 18:17:22 -0800

    All tests passing. Needs refactoring
    Created a monster of a recursive method while implementing a solution to this test. Will need to surround it with more tests to ensure correctness and then this might be a excellent opportunity for refactoring practice

--- day-5/transformRanges.go
+++ day-5/transformRanges.go
@@ -6,8 +6,58 @@ type SeedRange struct {
 	End int;
 }
 
+func shiftRange(r SeedRange, shift int) SeedRange {
+	return SeedRange{r.Start + shift, r.End + shift}
+}
+
+func rangeIntersect(r1, r2 SeedRange) SeedRange {
+	return SeedRange{max(r1.Start, r2.Start), min(r1.End, r2.End)}
+}
+
+func getOutputRangesRecursive(seeds SeedRange, mapSet MapSet, curr_ranges []SeedRange) []SeedRange {
+	if seeds.Start == seeds.End {
+		return curr_ranges
+	}
+
+	for i := 0; i<len(mapSet.Maps); i++ {
+		curr_map := mapSet.Maps[i]
+		curr_map_input_range := SeedRange{curr_map.SrcStart, curr_map.SrcStart + curr_map.Length}
+		if (rangesOverlap(seeds, curr_map_input_range)) {
+			//Intersect of overlap
+			intersect := rangeIntersect(seeds, curr_map_input_range)
+			curr_ranges = InsertAndMergeRange(
+				shiftRange(intersect, (curr_map.DestStart - curr_map.SrcStart)), 
+				curr_ranges,
+			)
+
+			//Minor optimization. Don't need to search the maps up to this point again
+			remainingMaps := MapSet{mapSet.Maps[i+1:]} 
+
+			//Left 
+			left := SeedRange{seeds.Start, intersect.Start}
+			curr_ranges = getOutputRangesRecursive(
+				left,
+				remainingMaps,
+				curr_ranges,
+			) 
+
+			//Right
+			right := SeedRange{intersect.End, seeds.End}
+			curr_ranges = getOutputRangesRecursive(
+				right,
+				remainingMaps,
+				curr_ranges,
+			)
+
+			return curr_ranges
+		}
+	}
+
+	return InsertAndMergeRange(seeds, curr_ranges)
+}
+
 func GetOutputRanges(seeds SeedRange, mapSet MapSet) []SeedRange {
-	return []SeedRange{}
+	return getOutputRangesRecursive(seeds, mapSet, []SeedRange{})
 }
 
 func min(x, y int) int {
@@ -28,14 +78,14 @@ func rangesOverlap(r1, r2 SeedRange) bool {
 	return r1.Start < r2.End && r2.Start < r1.End
 }
 
-func mergeOverlappingRange(r1, r2 SeedRange) SeedRange {
+func rangeUnion(r1, r2 SeedRange) SeedRange {
 	return SeedRange{min(r1.Start, r2.Start), max(r1.End, r2.End)}
 }
 
 func InsertAndMergeRange(new_range SeedRange, existing_ranges []SeedRange) []SeedRange {
 	for i := 0; i<len(existing_ranges); i++ {
 		if rangesOverlap(new_range, existing_ranges[i]){
-			merged_range := mergeOverlappingRange(new_range, existing_ranges[i])
+			merged_range := rangeUnion(new_range, existing_ranges[i])
 			existing_ranges[i] = existing_ranges[len(existing_ranges)-1]
 			return InsertAndMergeRange(
 				merged_range, 
