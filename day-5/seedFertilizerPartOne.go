57a41f2a13054d750aa7ac45794e5a1f5e2643b7
aidenbull <aidenbull@hotmail.com>
2024-02-12 11:30:24 -0800

    Added parsing and calculation time to solutions

--- day-5/seedFertilizerPartOne.go
+++ day-5/seedFertilizerPartOne.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func PrintPartOneSolution(input string) {
+	fmt.Println("\nPart one.")
+	parse_time_start := time.Now()
+
 	seedsAndMaps := ParseSeedsAndMaps(input)
+
+	fmt.Printf("Time to parse: %s\n", time.Since(parse_time_start))
+	calculation_time_start := time.Now()
 
 	lowestLocation := ApplyMultipleMaps(seedsAndMaps.Seeds[0], seedsAndMaps.MapSets)
 	for _, seed := range seedsAndMaps.Seeds[1:] {
@@ -15,5 +22,6 @@ func PrintPartOneSolution(input string) {
 		}
 	}
 
-	fmt.Println(lowestLocation)
+	fmt.Printf("Time to calculate: %s\n", time.Since(calculation_time_start))
+	fmt.Printf("Part one answer: %d\n", lowestLocation)
 }
--- day-5/seedFertilizerPartTwo.go
+++ day-5/seedFertilizerPartTwo.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func PrintPartTwoSolution(input string) {
+	fmt.Println("\nPart two.")
+	parse_time_start := time.Now()
+
 	seedsAndMaps := ParseSeedsAndMaps(input)
 	seedRanges := ProcessPartTwoSeeds(seedsAndMaps.Seeds)
+
+	fmt.Printf("Time to parse: %s\n", time.Since(parse_time_start))
+	calculation_time_start := time.Now()
+
 	curr_input_ranges := seedRanges
 	for _, mapSet := range(seedsAndMaps.MapSets) {
 		curr_input_ranges = TransformInputRangesToOutputRanges(curr_input_ranges, mapSet)
 	}
 	lowestLocation := SortRangesByIncreasingStart(curr_input_ranges)[0].Start
-	fmt.Println(lowestLocation)
+
+	fmt.Printf("Time to calculate: %s\n", time.Since(calculation_time_start))
+	fmt.Printf("Part two solution: %d\n", lowestLocation)
 }
