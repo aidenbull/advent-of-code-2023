58e9b9122d85630c23b6f706c7ba8b84f6b73b66
aidenbull <aidenbull@hotmail.com>
2024-02-12 11:01:56 -0800

    Solving Part 2
    Didn't realize that the question was asking for the lowest location. Thought it was asking for the input that corresponded to the lowest location. Ah well, seems all of inverseMapping isn't gonna be used

--- day-5/seedFertilizerPartTwo.go
+++ day-5/seedFertilizerPartTwo.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-	
+	"fmt"
 )
 
 func PrintPartTwoSolution(input string) {
-	//seedsAndMaps := ParseSeedsAndMaps(input)
+	seedsAndMaps := ParseSeedsAndMaps(input)
+	seedRanges := ProcessPartTwoSeeds(seedsAndMaps.Seeds)
+	curr_input_ranges := seedRanges
+	for _, mapSet := range(seedsAndMaps.MapSets) {
+		curr_input_ranges = TransformInputRangesToOutputRanges(curr_input_ranges, mapSet)
+	}
+	lowestLocation := SortRangesByIncreasingStart(curr_input_ranges)[0].Start
+	fmt.Println(lowestLocation)
 }
